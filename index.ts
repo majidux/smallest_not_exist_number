8cce6633ac59f47bbd1c1b74ae20a27eaf432f86
Majid Darvish nejad <46152564+majidux@users.noreply.github.com>
2021-02-26 12:31:35 +0330

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
 function solution(A) {
     const A2 = A.filter(item => item >= 1).sort((par1, par2) => par1 - par2);
-    console.log(A2);
     let foundNumber = 1;
     for (let i in A2) {
-      console.log(i);
       if (foundNumber < A2[i]) {
         return foundNumber;
       }
